de5bd98b906a2250156e4f11950c6f36f262c81e
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-08 09:03:02 +0100

    Refactoring

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
+    int count = 0;
+
     public MainPage()
     {
         InitializeComponent();
@@ -10,4 +12,14 @@ public partial class MainPage : ContentPage
     protected override void OnAppearing() { }
 
     protected override void OnDisappearing() { }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+    }
 }
